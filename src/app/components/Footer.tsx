211f4d668e09c581633115beb8f77e7b481d3ab3
mickykate <ka12miyuki@gmail.com>
2025-05-14 23:18:34 +0900

    Footer.tsxを編集。

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+    return (
+        <footer style={{textAlign: "center", padding: "1rem", background: "#008000", fontFamily: "rush Script MT', cursive", color: "#ffffff"}}>
+            Thanks for visiting my site!!
+        </footer>
+    )
+}
