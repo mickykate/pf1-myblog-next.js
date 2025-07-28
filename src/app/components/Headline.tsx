d34c1bbd580d145c0ba57a3a11ef64466a9985c5
mickykate <ka12miyuki@gmail.com>
2025-07-28 23:25:14 +0900

    ロゴを追加。

--- src/app/components/Headline.tsx
+++ src/app/components/Headline.tsx
@@ -1,7 +1,17 @@
 export function Headline() {
     return (
         <header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "1rem", position: "sticky", top: 0, backgroundColor: "#FFFEF7", color: "#000000", zIndex: 1000}}>
-            <div style={{ marginRight: "auto" }}>ロゴ</div>
+            <div style={{ marginRight: "auto" }}>
+              <a href="/">
+              <img src="/mylogo.jpeg" alt="My Logo"
+              style={{
+                width: "35px",
+                height: "35px",
+                borderRadius: "50%",
+                objectFit: "cover"
+              }}  />
+              </a>
+            </div>
             <nav style={{ display: "flex", gap: "2rem" }}>
                 <a href="#top" style={{ 
                   textDecoration: "none",
